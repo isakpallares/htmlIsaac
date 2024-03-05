beea194bd1861f790e3cb8c43726a1cc8f3a8505
Isaac Pallares <i.davidpallares@gmail.com>
2024-03-05 11:28:56 -0500

    made taller plus js

---
+++ JavaScript/js/variable.js
@@ -0,0 +1,11 @@
+let dia = 20;
+let nombre = "Juana";
+let altura = 1.92;
+let casado = false;
+document.write(dia);
+document.write("<br>")
+document.write(nombre);
+document.write("<br>")
+document.write(altura);
+document.write("<br>")
+document.write(casado);
