f23f56f5af578a825dd8eb24f78396d32b563840
Isaac Pallares <i.davidpallares@gmail.com>
2024-03-07 11:54:44 -0500

    comit

--- JavaScript/tallerclases/js/ej3.js
+++ JavaScript/tallerclases/js/ej3.js
@@ -1,13 +1,21 @@
-function Cliente(nombre, saldo){
-    this.nombre = nombre;
+class CuentaBancaria{
+    constructor(titular, saldo){
+    this.titular = titular;
     this.saldo = saldo;
     this.depositar = depositar;
     this.retirar = retirar;
+    this.obtenerSaldo = obtenerSaldo;
+    }
 }
 
 function depositar(valor){
     this.saldo = this.saldo + valor;
 }
+
 function retirar(valor){
     this.saldo = this.saldo - valor;
+}
+
+function obtenerSaldo(){
+    document.write("El saldo actual es de: "+this.saldo)
 }
