6e09337f91c2685d24e76b5dbf7c41282ead3c50
Isaac Pallares <i.davidpallares@gmail.com>
2024-05-30 11:56:38 -0500

    m

--- Django/tienda_online/gestionPedidos/models.py
+++ Django/tienda_online/gestionPedidos/models.py
@@ -9,7 +9,9 @@ class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField(max_length=20)
-
+    def __str__(self):
+        return f"{self.nombre}-{self.seccion}-{self.precio}"
+        
 class Pedidos(models.Model):
     n_pedido = models.IntegerField()
     fecha = models.DateField()
