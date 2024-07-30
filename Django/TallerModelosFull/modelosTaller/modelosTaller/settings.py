5b99ded487e52d8184ed9e6e43b4cfb209d1e018
isakpallares <i.davidpallares@gmail.com>
2024-07-30 11:05:41 -0500

    pip instalations

--- Django/TallerModelosFull/modelosTaller/modelosTaller/settings.py
+++ Django/TallerModelosFull/modelosTaller/modelosTaller/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'modelo_taller',
         'USER': 'root',
-        'PASSWORD': 'Isaac271204',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT' : '3306',
     }
